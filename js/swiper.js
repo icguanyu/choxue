750169a76d2e1aac0ada42293b42201594a1854a
Nick <icguanyu@gmail.com>
2019-12-22 22:48:21 +0800

    [new]更新(改版) 即時賽況 #live-game (Swiper.js)

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,11 @@
-/* 首頁頂 */
+/* 
+Swiper5
+以下為全站使用到的所有 swiper 元件初始化與設定檔
+API文件 https://www.swiper.com.cn/
+by Nick 2019-12
+*/
+
+/* 首頁-Banner #top-carousel*/
 var mySwiper = new Swiper('#top-carousel', {
   loop: true,
   pagination: {
@@ -8,5 +15,25 @@ var mySwiper = new Swiper('#top-carousel', {
   navigation: {
     nextEl: '#top-carousel .swiper-button-next',
     prevEl: '#top-carousel .swiper-button-prev',
+  }
+});
+/* 首頁-即時賽況 #live-game*/
+var mySwiper = new Swiper('#live-game', {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '#top-carousel .swiper-button-next',
+    prevEl: '#top-carousel .swiper-button-prev',
+  },
+  breakpoints: {
+    1330: {
+      slidesPerView: 4,
+    },
+    1080: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
+    }
   }
 });
