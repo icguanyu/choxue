a9a6628e4b18b135a42aab67f57598ca4a55a8b3
Nick <icguanyu@gmail.com>
2020-01-15 12:43:43 +0800

    [update]rwd lock scorllbar/menu active

--- js/layout.js
+++ js/layout.js
@@ -9,10 +9,18 @@
     e.stopPropagation()
     $hamburger.toggleClass('active')
     $mobile_menu.toggleClass('active')
+    $('html, body').css({
+      overflow: 'hidden',
+      height: '100%'
+    });
   }
   function hideMobileMenu(e) {
     $hamburger.removeClass('active')
     $mobile_menu.removeClass('active')
+    $('html, body').css({
+      overflow: 'auto',
+      height: 'auto'
+    });
   }
 
   $hamburger.click(toggleMobileMenu)
